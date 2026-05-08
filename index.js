7f942dd31b143f3ab233eb14746048f2ed961a6c
j-marcos-b <correodemarcosb@gmail.com>
2026-05-07 21:47:58 -0300

    agregar seguridad con helmet y cors, e implementar versionado de API

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 import 'dotenv/config';
 import express from 'express';
 import morgan from 'morgan';
+import cors from 'cors';
+import helmet from 'helmet';
 import './config/db.js';
 import pacientesRoutes from './routes/pacientes.routes.js';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(helmet()); // Protege las cabeceras HTTP
+app.use(cors());   // Habilita peticiones desde otros dominios
 app.use(express.json()); 
 app.use(morgan('dev'));
 
@@ -14,7 +18,7 @@ app.get('/', (req, res) => {
     res.json({ mensaje: 'Bienvenido a la API de la Clínica Médica' });
 });
 
-app.use('/api/pacientes', pacientesRoutes);
+app.use('/api/v1/pacientes', pacientesRoutes); // Agregamos "v1" para el versionado
 
 app.listen(PORT, () => {
     console.log(`Servidor corriendo en http://localhost:${PORT}`);
